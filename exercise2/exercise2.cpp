8a11b7c622c09696a3555ee94941f2cf4e2f4c80
badai ngasubala <badaingasubala12@gmail.com>
2023-05-12 15:05:45 +0700

    Add project files.

---
+++ exercise2/exercise2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[108];  ////////sudah berusaha semaksimal mungkin hasilnya seperti ini maafkan saya
+int n;
+
+
+void input() {
+
+}
+
+
+
+
+
